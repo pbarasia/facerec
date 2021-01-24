ee7b8f2d00da4c247232749b90aaf46824ccb69e
pbarasia <pbarasia@gmail.com>
2021-01-24 20:50:55 +0100

    integrate basic vuex store

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import Main from './components/Main.vue'
 import '../css/app.css'
 import Vue from 'vue'
+import store from './store'
 
 Vue.component('main-component', Main);
 
 new Vue({
   el: '#app',
-  template:'<main-component></main-component>'
+  template:'<main-component></main-component>',
+  store: store
 })
